14dfbb28322557191671437ff7de597ef450f9ed
ralvescosta <rafael.rac.mg@gmail.com>
2023-11-22 22:47:41 -0300

    feat: container

--- go.mod
+++ go.mod
@@ -1,3 +1,64 @@
 module github.com/tointernet/edgar
 
 go 1.21.4
+
+require (
+	github.com/ralvescosta/gokit/configs v1.16.0
+	github.com/ralvescosta/gokit/configs_builder v1.16.0
+	github.com/ralvescosta/gokit/logging v1.16.0
+	github.com/ralvescosta/gokit/metrics v1.16.0
+	github.com/ralvescosta/gokit/mqtt v1.16.0
+	github.com/ralvescosta/gokit/tiny_http v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.17.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/ralvescosta/dotenv v1.0.4 // indirect
+	github.com/stretchr/objx v0.5.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.44.0 // indirect
+	go.opentelemetry.io/otel/exporters/prometheus v0.44.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231120223509-83a465c0220f // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace github.com/ralvescosta/gokit/mqtt => ../gokit/mqtt
+
+replace github.com/ralvescosta/gokit/metrics => ../gokit/metrics
+
+replace github.com/ralvescosta/gokit/configs => ../gokit/configs
+
+replace github.com/ralvescosta/gokit/configs_builder => ../gokit/configs_builder
+
+replace github.com/ralvescosta/gokit/tiny_http => ../gokit/tiny_http
--- pkg/container.go
+++ pkg/container.go
@@ -1,1 +1,122 @@
 package pkg
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ralvescosta/gokit/configs"
+	configsbuilder "github.com/ralvescosta/gokit/configs_builder"
+	"github.com/ralvescosta/gokit/logging"
+	"github.com/ralvescosta/gokit/metrics"
+	"github.com/ralvescosta/gokit/mqtt"
+	tinyHttp "github.com/ralvescosta/gokit/tiny_http"
+)
+
+type Container struct {
+	cfgs               *configs.Configs
+	logger             logging.Logger
+	sig                chan os.Signal
+	mqttClient         mqtt.MQTTClient
+	prometheus         metrics.PrometheusMetrics
+	prometheusShotdown func(context.Context) error
+	tinyHTTPServer     tinyHttp.TinyServer
+}
+
+func NewContainer() (*Container, error) {
+	cfgs, logger, err := provideConfigsAndLogger()
+	if err != nil {
+		return nil, err
+	}
+
+	prometheusProvider, shutdown, err := providePrometheus(cfgs, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	sig, tinyServer, err := provideTinyServer(cfgs, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	mqttClient, err := provideMQTTClient(cfgs, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Container{
+		cfgs:               cfgs,
+		logger:             logger,
+		sig:                sig,
+		mqttClient:         mqttClient,
+		prometheus:         prometheusProvider,
+		prometheusShotdown: shutdown,
+		tinyHTTPServer:     tinyServer,
+	}, nil
+}
+
+func provideConfigsAndLogger() (*configs.Configs, logging.Logger, error) {
+	cfgs, err := configsbuilder.NewConfigsBuilder().
+		MQTT().
+		Metrics().
+		Build()
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	logger, err := logging.NewDefaultLogger(cfgs.AppConfigs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return cfgs, logger, nil
+}
+
+func providePrometheus(cfgs *configs.Configs, logger logging.Logger) (metrics.PrometheusMetrics, func(context.Context) error, error) {
+	logger.Debug("creating prometheus provider...")
+
+	prometheusProvider := metrics.NewPrometheus(cfgs, logger)
+	shutdown, err := prometheusProvider.Provider()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	logger.Debug("prometheus provider created!")
+
+	return prometheusProvider, shutdown, nil
+}
+
+func provideTinyServer(cfgs *configs.Configs, logger logging.Logger) (chan os.Signal, tinyHttp.TinyServer, error) {
+	logger.Debug("creating signal channel...")
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	logger.Debug("signal channel created!")
+
+	logger.Debug("creating tiny http server...")
+
+	tinyServer := tinyHttp.NewTinyServer(cfgs.HTTPConfigs, logger).
+		Sig(sig).
+		Prometheus()
+
+	logger.Debug("tiny http server created!")
+
+	return sig, tinyServer, nil
+}
+
+func provideMQTTClient(cfgs *configs.Configs, logger logging.Logger) (mqtt.MQTTClient, error) {
+	logger.Debug("connecting to mqtt...")
+
+	mqttClient := mqtt.NewMQTTClient(cfgs, logger)
+	err := mqttClient.Connect()
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Debug("mqtt connected!")
+
+	return mqttClient, err
+}
